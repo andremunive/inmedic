866722b7214cc5faa47aa6d61b87136baa83fd17
damn21 <dmezae@uninorte.edu.co>
2022-05-13 01:24:26 -0500

    feat: ✨  endpoints users added

--- src/components/user/UserController.js
+++ src/components/user/UserController.js
@@ -1,6 +1,6 @@
 const Doctor = require('../doctor/DoctorModel');
 const Client = require('../client/ClientModel');
-const { generateAccessToken } = require('../../helpers/jwt');
+const { generateAccessToken, toInvalidTokens } = require('../../helpers/jwt');
 const ApiError = require('../../utils/ApiError');
 const bcrypt = require("bcryptjs");
 const AuthSerializer = require("../../Serializers/AuthSerializer");
@@ -13,6 +13,25 @@ const findUser = async (where) => {
     const doctor = await Doctor.findOne( where );
     console.log("USER_DOCTOR",doctor);
     const client = await Client.findOne( where );
+    console.log("USER_CLIENT",client);
+    if (doctor) { 
+        return doctor;
+    } else{
+        if(client) {
+            return client;
+        } else {
+            throw new ApiError('User not found 1', 400);
+        }   
+    }
+};
+
+const findUserId = async (where) => {
+    console.log("WHERE",where);
+    Object.assign(where, { active: true });
+   
+    const doctor = await Doctor.findById( where );
+    console.log("USER_DOCTOR",doctor);
+    const client = await Client.findById( where );
     console.log("USER_CLIENT",client);
     if (doctor) { 
         return doctor;
@@ -67,9 +86,12 @@ const updateUser = async (req, res, next) => {
     try {
         const {params, body} = req;
 
-        const userID = Number(params._id);
+        const userId = String(params._id);
+        console.log("USERid: ",userId);
+        req.isUserAuthorized(userId);
 
-        const user = await findUser(userID);
+        const user = await findUserId({_id: userId});
+        console.log("USER 2", user);
 
         const userPayload = {
             email: body.email,
@@ -92,12 +114,45 @@ const updateUser = async (req, res, next) => {
     } catch (err) {
         next(err);
     }
+};
+
+const deactivateUser = async (req, res, next) => {
+    try {
+      const { params } = req;
+  
+      const userId = String(params._id);
+      console.log("USER_ID: ", userId);
+      req.isUserAuthorized(userId);
+  
+      const user = await findUserId({ _id: userId });
+  
+      Object.assign(user, { active: false });
+  
+      await user.save();
+  
+      res.json(new UserSerializer(null));
+    } catch (err) {
+      next(err);
+    }
+  };
 
 
-}
+const logOut = async (req, res, next) => {
+    try {
+        const user = await findUserId({_id: req.user.id});
+        console.log("UserLogout: ",user);
+        const accessToken = req.headers.authorization ? req.headers.authorization.split(' ')[1] : "";
+        toInvalidTokens(accessToken);
+        res.json(new UserSerializer(user));
+      } catch (err) {
+        next(err);
+      }
+    };
 
 
 module.exports = {
     login,
-    updateUser
+    updateUser,
+    logOut,
+    deactivateUser
 }
