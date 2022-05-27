acf27804cbce2cde5f2f4c8ed27a4ad52988a9e4
bdelgado1 <bdelgado@getontop.com>
2022-05-27 15:18:47 -0500

    endpoint consultar cita

--- src/components/client/ClientController.js
+++ src/components/client/ClientController.js
@@ -274,10 +274,37 @@ const ReviewDoctor = async(req, res, next) => {
 
 };
 
+const getAppointmentsByDoctorId = async(req, res, next) => {
+
+    try {
+        const { body } = req;
+
+        req.isRole('user');
+
+        const doctorId = req.params.doctorId;
+
+        console.log("doctor id: "+doctorId)
+
+        const appointments = await Schedule.find({ idDoctor: doctorId });
+        
+        if (!appointments) {
+            throw new ApiError("Doctor not found", 400);
+        }
+
+        res.status(200).json(appointments);
+        
+    } catch (err) {
+        next(err);
+    }
+
+
+};
+
 module.exports = {
     clientSignup,
     GetServices,
     ProfileDoctor,
     ReviewDoctor,
-    AgendarCita
+    AgendarCita,
+    getAppointmentsByDoctorId
 };
