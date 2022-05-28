d450c71d2c1c0c00c3e7bb31ced89814147e8fd2
damn21 <dmezae@uninorte.edu.co>
2022-05-27 21:14:49 -0500

    🔧 endpoint updated

--- src/components/doctor/DoctorController.js
+++ src/components/doctor/DoctorController.js
@@ -10,18 +10,16 @@ const doctorSignUp = async(req, res, next) => {
 
     try {
 
-        const input = { name, lastName, documentNumber, professionalCard, phoneNumber, address, city, perfil, gender,
-             email, password, birthDate, specialization, services} = req.body;
-        let inputArray = Object.keys(input);
+        var data = req.body;
+        console.log('DATA => ', req.body)
 
-        // Validate user input
-        if (inputArray.every((ele) => input[ele] != "" && input[ele] != undefined)) {} else {
+        if (!data) {
             throw new ApiError("All input is required", 400);
         }
 
         // Validamos la existencia del usuario en la base de datos
         let oldUser;
-        await Doctor.findOne({ email: email }).then((doctor) => {
+        await Doctor.findOne({ email: data.email }).then((doctor) => {
 
             oldUser = doctor;
         }).catch((error) => {
@@ -34,47 +32,21 @@ const doctorSignUp = async(req, res, next) => {
 
         // Encrypt user password
         let encryptedPassword;
-        await bcrypt.hash(password, 10).then((resul) => {
+        await bcrypt.hash(data.password, 10).then((resul) => {
             encryptedPassword = resul
-            password = encryptedPassword
+            data.password = encryptedPassword
         }).catch((error) => {
             console.log("Encripting Error", error)
-        });
-
-        //const image = await cloudinary.v2.uploader.upload(perfil);
-        //console.log(image);
-
-        let _doctor;
-        await Doctor.create({
-            name,
-            lastName,
-            documentNumber,
-            professionalCard,
-            phoneNumber,
-            address,
-            city,
-            perfil,
-            gender,
-            email,
-            password: encryptedPassword,
-            birthDate,
-            specialization,
-            services,
-        }).then((doctor) => {
-            _doctor = doctor
-            res.status(200).json(_doctor);
-        }).catch((error) => {
-            console.log(error)
         })
 
-        // let _doctor;
-        // await Doctor.create(data)
-        //     .then((doctor) => {
-        //         _doctor = doctor
-        //         res.status(200).json(_doctor);
-        //     }).catch((error) => {
-        //         console.log(error)
-        //     });
+        let _doctor;
+        await Doctor.create(data)
+            .then((doctor) => {
+                _doctor = doctor
+                res.status(200).json(_doctor);
+            }).catch((error) => {
+                console.log(error)
+            });
 
     } catch (err) {
         next(err);
@@ -102,15 +74,15 @@ const Addconsult = async(req, res, next) => {
         if (!doctor) throw new ApiError("User not found", 400);
 
 
-        //console.log("URL: ",doctor.perfil);
-        //const image = await cloudinary.v2.uploader.upload(req.file.perfil);
+        console.log("URL: ",doctor.perfil);
+        const image = await cloudinary.v2.uploader.upload(req.file.perfil);
         //const image = await cloudinary.v2.uploader.upload(body.perfil);
 
         //console.log(image);
 
         let consult = new ConsultSchema({
             idDoctor: doctor._id,
-            perfil: doctor.perfil,
+            perfil: image.url,
             name: doctor.name + " " + doctor.lastname,
             description: body.description,
             description2: body.description2,
